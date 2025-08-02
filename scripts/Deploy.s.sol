68fce6c2febe98e773f6ef550f43f2c513f5155c
Arya Singh <singharya2209@gmail.com>
2025-08-02 22:27:50 +0530

    Fix forge-std import path in deployment script

--- scripts/Deploy.s.sol
+++ scripts/Deploy.s.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import "forge-std/Script.sol";
+import "../lib/forge-std/src/Script.sol";
 import "../src/SmartWallet.sol";
 import "../src/Paymaster.sol";
 import "../src/interfaces/IEntryPoint.sol";
